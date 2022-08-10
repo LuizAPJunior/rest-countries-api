64fb77fa1aa04647cad712c19c0f4b2e77d1768e
LuizAPJunior <luiz.alpjr@gmail.com>
2022-08-10 02:18:57 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <p>hello world !</p>
+    </div>
+  );
+};
+
+export default App;

aef7054942345ebaefb4c722bc3f6a00f8a223dc
LuizAPJunior <luiz.alpjr@gmail.com>
2022-10-07 22:07:58 -0300

    add async/await support in some browsers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
 import App from './App';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
